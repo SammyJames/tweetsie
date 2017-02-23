cbb600d943b054849048688fca4039b990f1eecd
Sammy James <sammy.morgan.james+github@gmail.com>
2017-02-23 09:12:22 -0500

    Adding User and Local User types.

---
+++ src/local_user.rs
@@ -0,0 +1,38 @@
+
+
+use std::string::String;
+
+use user::TwitterUser;
+use user::User;
+
+#[derive(Debug)]
+pub struct LocalUser {
+	consumer_key: String,
+	consumer_secret: String,
+	token: String,
+	token_secret: String,
+	user: User,
+}
+
+impl LocalUser {
+	pub fn new() -> LocalUser {
+
+		LocalUser {
+			consumer_key: String::from(""),
+			consumer_secret: String::from(""),
+			token: String::from(""),
+			token_secret: String::from(""),
+			user: User::new(),
+		}
+	}
+}
+
+impl TwitterUser for LocalUser {
+	fn get_display_name(&self) -> &String {
+		self.user.get_display_name()
+	}
+
+	fn get_handle(&self) -> &String {
+		self.user.get_handle()
+	}
+}
---
+++ src/user.rs
@@ -0,0 +1,34 @@
+
+use std::string::String;
+
+#[derive(Debug)]
+pub struct User {
+	display_name: String,
+	handle: String,
+}
+
+
+impl User {
+	pub fn new() -> User {
+
+		User {
+			display_name: String::from("Display Name"),
+			handle: String::from("@handle"),
+		}
+	}
+}
+
+pub trait TwitterUser {
+	fn get_display_name(&self) -> &String;
+	fn get_handle(&self) -> &String;
+}
+
+impl TwitterUser for User {
+	fn get_display_name(&self) -> &String {
+		 &self.display_name
+	}
+
+	fn get_handle(&self) -> &String {
+		 &self.handle
+	}
+}
