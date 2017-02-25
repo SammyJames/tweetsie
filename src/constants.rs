fd692908d4c55f81b35be454573de004dababce0
Sammy James <sammy.morgan.james+github@gmail.com>
2017-02-24 19:19:59 -0500

    add consumer key

--- src/constants.rs
+++ src/constants.rs
@@ -4,6 +4,7 @@ use std::string::String;
 
 lazy_static! {
 	pub static ref CONSUMER_SEC: SecStr = SecStr::from(include_str!("../res/secret"));
+	pub static ref CONSUMER_KEY: SecStr = SecStr::from(include_str!("../res/key"));
 	pub static ref APP_ONLY_AUTH: String = String::from("https://api.twitter.com/oauth2/token");
 	pub static ref REQ_TOKEN_URL: String = String::from("https://api.twitter.com/oauth/request_token");
 	pub static ref AUTHORIZE_URL: String = String::from("https://api.twitter.com/oauth/authorize");
